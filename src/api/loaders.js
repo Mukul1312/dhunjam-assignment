26f597cbab999d2805e6f9cd7fd09b1a549b8f3d
Mukul dubey <mdubey.0433@gmail.com>
2023-11-17 23:49:32 +0530

    admin details get api integrated and mapped in ui

--- src/api/loaders.js
+++ src/api/loaders.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
-const adminDetailsLoader = async ({ request, params }) => {
+const adminDetailsLoader = async ({ params }) => {
   // Fetching data from an API
-  console.log("params", params);
-  console.log("request", request);
-  // const data = await axios.get("https://jsonplaceholder.typicode.com/users");
-  return {
-    id: 1,
-    name: "Admin",
-    email: "",
-  };
+  const { id } = params;
+
+  try {
+    const response = await axios.get(`https://stg.dhunjam.in/account/admin/${id}`);
+
+    if(response.status === 200 && response.data.status === 200 && response.data.response.toLowerCase() === "success") {
+        return response.data.data;
+    } else {
+      alert(response.data.ui_err_msg);
+    }
+  } catch (error) {
+    console.log(error);
+    alert(error);
+  }
+
 };
 
 export { adminDetailsLoader };
+
+
--- src/pages/AdminDetails.jsx
+++ src/pages/AdminDetails.jsx
@@ -1,7 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+
 const AdminDetails = () => {
+  const data = useLoaderData();
+
+  if(data === undefined) {
+    return <h1>Something went wrong</h1>
+  }
+
   return (
-    <div>admin details</div>
+    <>
+      <h1>Admin Details</h1>
+      <p>{data.name}, {data.location} on Dhun Jam</p>
+      <p>Do you want to charge your customers for requesting songs?</p> {data.charge_customers ? "Yes" : "No"}
+      {data.charge_customers && <p>Custom song request amount-?</p>} {data.charge_customers && <p>Category 6: {data.amount.category_6}</p>}
+      {data.charge_customers && <p>Regular song request amounts, from high to low</p>} {data.charge_customers && <p>Category 7: {data.amount.category_7}</p>} {data.charge_customers && <p>Category 8: {data.amount.category_8}</p>} {data.charge_customers && <p>Category 9: {data.amount.category_9}</p>} {data.charge_customers && <p>Category 10: {data.amount.category_10}</p>}
+      <button>Save</button>
+    </>
   )
 }
 
 export default AdminDetails
+
